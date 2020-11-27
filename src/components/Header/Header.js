5c22e694c52a9cd63e7e4333f9af90a3ba161f30
George Hawthorne <bluemonkey1130@gmail.com>
2020-11-27 22:54:27 +0000

    slimmed down the grid SCSS
    -removed some of the classes required to set up grid
    -set the default layout to none fixed widths
    
    Signed-off-by: George Hawthorne <bluemonkey1130@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,56 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentWillMount() {
-
-    }
-
-    componentDidMount() {
-
-    }
-
-    componentWillReceiveProps(nextProps) {
-
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-
-    }
-
-    componentWillUnmount() {
-
-    }
 
     render() {
-        let rowWidth = null ? this.props.content.width  : 'standard';
-
+        let rowWidth =  this.props.content.width;
         return (
             <header id="header" className="grid-row">
                 <div className={["grid-layout", rowWidth, "has-one-column"].join(' ')}>
-                    <div className="cluster">
-                        <img className="logo" src={this.props.content.img} alt="Text"/>
-                        <nav className="cluster">
-                            <ul>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">Shop</a></li>
-                                <li><a href="#">Contact</a></li>
-                                <li><a href="#">Accessibility</a></li>
-                            </ul>
-                        </nav>
+                    <div className="cover">
+                        <div className="cluster">
+                            <img className="logo" src={this.props.content.img} alt="Text"/>
+                            <nav className="cluster">
+                                <ul>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a href="#">Accessibility</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <h1 className="align-center">{this.props.content.reallyShortText}</h1>
+                        <p className="align-center">{this.props.content.shortText}</p>
                     </div>
                 </div>
             </header>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,10 +5,13 @@ import Header from "../components/Header/Header";
 class App extends Component {
     state = {
         header: {
-            width: 'standard',
+            width: 'alignfull',
             img: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
-        },
+            reallyShortText: 'Radio buy raise speak.',
+            shortText: 'More such food reveal ok improve similar.',
 
+
+        },
         img: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80',
         reallyLongText: 'Skin base work seat now stay than. Successful itself interest radio economy maybe around. Agent explain lawyer film daughter. Green direction down ever. Remember seat imagine. Else case law. Far long indicate suggest police dream main. Travel hold explain lead into. Fight certainly case popular bank cup type. Enim ut sapiente voluptates distinctio nam minima. Ab mollitia non cupiditate. Quasi consequatur rerum eius ea officia Employer longtemps immense examiner. Permettre dos jeune puissant durant étouffer. Envoyer goût secrétaire céder. Ipsum deleniti omnis quae est saepe perspiciatis. Fugiat tempore voluptas ab. Ea quis aspernatur laboriosam in. Recevoir éclat fatiguer note envie. Midi tempête quatre expérience société taire. Posséder accord arracher pluie. Autorité vieux fin. Celui éprouver sueur ennemi poète marché. Depuis rose réponse me maître engager. Libero sit maiores delectus. Ullam non quaerat. Ipsum accusantium expedita atque alias recusandae. Commodi esse tempore veritatis quaerat assumenda. Assumenda harum dicta officiis commodi quas unde illum. Quod vero ullam dignissimos harum voluptates.',
         longText: 'Puis contre forme fait avancer absolu lumière. Semblable accent sourire agiter parti. Ci ouvrir savoir plonger santé caractère simple. Totam voluptatem voluptates ducimus. Debitis et in ipsa. Officia ullam soluta architecto eveniet neque repellat. Hic fuga ab repudiandae. Occaecati laudantium dicta occaecati error deserunt fugit ullam. Laudantium vero.',
@@ -21,8 +24,9 @@ class App extends Component {
         return (
             <Fragment>
                 <Header content={this.state.header}/>
+
                 <article className="grid-row">
-                    <section className="grid-layout standard has-one-column">
+                    <section className="grid">
                         <div className="stack align-center">
                             <figure className="frame">
                                 <img src={this.state.img} alt="Text"/>
@@ -34,7 +38,7 @@ class App extends Component {
                             </div>
                         </div>
                     </section>
-                    <section className="grid-layout standard has-three-columns">
+                    <section className="grid grid-gap-900">
                         <figure className="frame">
                             <img src={this.state.img} alt="Text"/>
                         </figure>
@@ -45,7 +49,7 @@ class App extends Component {
                             <img src={this.state.img} alt="Text"/>
                         </figure>
                     </section>
-                    <section className="grid-layout standard has-two-columns">
+                    <section className="grid">
                         <div>
                             <h3>{this.state.shortText}</h3>
                             <p>{this.state.longText}</p>
@@ -70,7 +74,7 @@ class App extends Component {
                             </div>
                         </form>
                     </section>
-                    <section className="grid-layout standard has-two-columns">
+                    <section className="grid">
                         <div className="stack stack-small">
                             <figure className="frame">
                                 <img src={this.state.img} alt="Text"/>
@@ -83,7 +87,7 @@ class App extends Component {
                             <p>{this.state.text}</p>
                         </div>
                     </section>
-                    <section className="grid-layout standard has-two-columns rightwide">
+                    <section className="grid-layout has-two-columns rightwide">
                         <div className="box">
                             <h4>{this.state.shortText}</h4>
                             <p>{this.state.text}</p>
@@ -93,7 +97,7 @@ class App extends Component {
                             <p>{this.state.longText}</p>
                         </div>
                     </section>
-                    <section className="grid-layout standard grid-gap-400  has-two-columns">
+                    <section className="grid">
                         <div>
                             <h3>{this.state.shortText}</h3>
                             <h5>{this.state.shortText}</h5>
@@ -114,6 +118,55 @@ class App extends Component {
                                         <input type="text"/>
                                         <input className="button right" type="submit" value="Submit"/>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="grid">
+                        <div><p>{this.state.text}</p></div>
+                        <div><p>{this.state.text}</p></div>
+                        <div><p>{this.state.text}</p></div>
+                    </section>
+                    <section className="grid">
+                        <div className="switcher">
+                            <div>
+                                <div><p>{this.state.text}</p></div>
+                                <div><p>{this.state.text}</p></div>
+                                <div><p>{this.state.text}</p></div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="grid">
+                        <div className="box invert">
+                            <div className="stack">
+                                <div className="frame">
+                                    <img src={this.state.img} alt="Text"/>
+                                </div>
+                                <div>
+                                    <h4>{this.state.reallyShortText}</h4>
+                                    <p>{this.state.text}.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="box">
+                            <div className="stack">
+                                <div className="frame">
+                                    <img src={this.state.img} alt="Text"/>
+                                </div>
+                                <div>
+                                    <h4>{this.state.reallyShortText}</h4>
+                                    <p>{this.state.text}.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="box invert">
+                            <div className="stack">
+                                <div className="frame">
+                                    <img src={this.state.img} alt="Text"/>
+                                </div>
+                                <div>
+                                    <h4>{this.state.reallyShortText}</h4>
+                                    <p>{this.state.text}.</p>
                                 </div>
                             </div>
                         </div>
